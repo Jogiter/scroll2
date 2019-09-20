c5bf93bd9043c49007ff5ff89b02f35427fa993f
jogiter <jogiter.g@gmail.com>
2019-09-20 11:12:51 +0800

    test(puppeteer): add puppeteer and setup env

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,17 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   parserOptions: {
     sourceType: 'module'
   },
   parser: 'babel-eslint',
   // https://github.com/standard/eslint-config-standard
-  extends: 'standard',
+  extends: [
+    'standard'
+  ],
+  plugins: ["jest"],
   rules: {
     indent: ["error", 2],
     quotes: ["error", "single"],
--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-// TODO:
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
+  });
+});
